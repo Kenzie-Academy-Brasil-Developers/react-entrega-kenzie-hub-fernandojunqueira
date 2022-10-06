cc64ca0c850be8ce1eb7ea661970974d033a5a0c
Fernandojunqueira <fernandojunqueira12@gmail.com>
2022-10-05 21:31:47 -0300

    feat: add style and html dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
 
 function App() {
   return (
     <>
-      <LoginPage/>
+      {/* <LoginPage/> */}
+      <RegisterPage/>
     </>
   );
 }
